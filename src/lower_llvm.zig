ea430d0317f89c366d0f3717830389cc0d59a34d
olwang <yuesefa@gmail.com>
2026-07-30 20:13:45 -0700

    Read LLVM enum literals from MIR facts

--- src/lower_llvm.zig
+++ src/lower_llvm.zig
@@ -9034,6 +9034,10 @@ const LlvmEmitter = struct {
                 if (self.mirTargetTypeFactAt(.string_literal, expr.span)) |fact| fact.target_ty else null
             else
                 null,
+            .enum_literal => if (expr.span.line != 0 and expr.span.column != 0)
+                if (self.mirTargetTypeFactAt(.enum_literal, expr.span)) |fact| fact.target_ty else null
+            else
+                null,
             .float_literal => if (expr.span.line != 0 and expr.span.column != 0)
                 if (self.mirTargetTypeFactAt(.float_literal, expr.span)) |fact| fact.target_ty else null
             else
