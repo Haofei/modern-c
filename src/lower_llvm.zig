7ad5b24788468ae63fc5b37e431048450ddc4ebe
olwang <yuesefa@gmail.com>
2026-07-30 19:57:14 -0700

    Read LLVM char literals from MIR facts

--- src/lower_llvm.zig
+++ src/lower_llvm.zig
@@ -9026,6 +9026,10 @@ const LlvmEmitter = struct {
                 if (self.mirTargetTypeFactAt(.expression_result, expr.span)) |fact| fact.target_ty else null
             else
                 null,
+            .char_literal => if (expr.span.line != 0 and expr.span.column != 0)
+                if (self.mirTargetTypeFactAt(.char_literal, expr.span)) |fact| fact.target_ty else null
+            else
+                null,
             .float_literal => if (expr.span.line != 0 and expr.span.column != 0)
                 if (self.mirTargetTypeFactAt(.float_literal, expr.span)) |fact| fact.target_ty else null
             else
